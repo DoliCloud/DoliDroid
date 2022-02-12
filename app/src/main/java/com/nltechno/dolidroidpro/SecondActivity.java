e028beb2d060abe690d61f0741e394b6f9f2b9b7
Laurent Destailleur <eldy@destailleur.fr>
2022-02-12 14:09:35 +0100

    Fix link to emails

--- app/src/main/java/com/nltechno/dolidroidpro/SecondActivity.java
+++ app/src/main/java/com/nltechno/dolidroidpro/SecondActivity.java
@@ -1485,13 +1485,76 @@ public class SecondActivity extends Activity {
             } else if (url.startsWith("mailto:")) {  // Intercept mailto urls
                 Log.d(LOG_TAG, "Launch mailto : " + url);
                 try {
+                    String theEmail = "", theEmailCC = "", theEmailBCC = "", theSubject = "", theBody = "";
+                    Boolean hasEmail = true, hasEmailCC = url.contains("&cc="), hasEmailBCC = url.contains("&bcc="), hasSubject = url.contains("&subject="), hasBody = url.contains("&body=");
+                    int posEmail = 0, posEmailCC = hasEmailCC ? url.indexOf("&cc=") : 0, posEmailBCC = hasEmailBCC ? url.indexOf("&bcc=") : 0, posSubject = hasSubject ? url.indexOf("&subject=") : 0, posBody = hasBody ?  url.indexOf("&body=") : 0;
+
+                    if (hasEmail && hasEmailCC) {
+                        theEmail = url.substring(posEmail, posEmailCC - posEmail);
+                    } else if (hasEmail && hasEmailBCC) {
+                        theEmail = url.substring(posEmail, posEmailBCC - posEmail);
+                    } else if (hasEmail && hasSubject) {
+                        theEmail = url.substring(posEmail, posSubject - posEmail);
+                    } else if (hasEmail && hasBody) {
+                        theEmail = url.substring(posEmail, posBody - posEmail);
+                    } else if (hasEmail) {
+                        theEmail = url;
+                    } else {
+                        /*theEmail    = url;*/
+                    }
+
+                    if (hasEmailCC && hasEmailBCC) {
+                        theEmailCC = url.substring(posEmailCC, posEmailBCC - posEmailCC);
+                    } else if (hasEmailCC && hasSubject) {
+                        theEmailCC = url.substring(posEmailCC, posSubject - posEmailCC);
+                    } else if (hasEmailCC && hasBody) {
+                        theEmailCC = url.substring(posEmailCC, posBody - posEmailCC);
+                    } else if (hasEmailCC) {
+                        theEmailCC = url.substring(posEmailCC);
+                    } else {
+                        /*theEmailCC  = url.substring(posEmailCC);*/
+                    }
+                    theEmailCC = theEmailCC.replace("&cc=", "");
+
+                    if (hasEmailBCC && hasSubject) {
+                        theEmailBCC = url.substring(posEmailBCC, posSubject - posEmailBCC);
+                    } else if (hasEmailBCC && hasBody) {
+                        theEmailBCC = url.substring(posEmailBCC, posBody - posEmailBCC);
+                    } else if (hasEmailBCC) {
+                        theEmailBCC = url.substring(posEmailBCC);
+                    } else {
+                        /*theEmailBCC = url.substring(posEmailBCC);*/
+                    }
+                    theEmailBCC = theEmailBCC.replace("&bcc=", "");
+
+                    if (hasSubject && hasBody) {
+                        theSubject = url.substring(posSubject, posBody - posSubject);
+                    } else if (hasSubject) {
+                        theSubject = url.substring(posSubject);
+                    } else {
+                        /*theSubject  = url.substring(posSubject);*/
+                    }
+                    theSubject = theSubject.replace("&subject=", "");
+
+                    if (hasBody) {
+                        theBody = url.substring(posBody);
+                    } else {
+                        /*theBody     = url.substring(posBody);*/
+                    }
+                    theBody = theBody.replace("&body=", "");
+                    theSubject = theSubject.replace("%20", " ");
+                    theBody = theBody.replace("%20", " ").replace("%0A", "\n");
+                    String recipient = url.substring(url.indexOf(":") + 1);
+
                     Intent emailIntent = new Intent(Intent.ACTION_SEND, Uri.parse(url));
                     emailIntent.setDataAndType(Uri.parse(url), "message/rfc822");
-                    String recipient = url.substring(url.indexOf(":") + 1);
+                    emailIntent.setType("message/rfc822");
                     emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{recipient});
-                    //emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, mContext.getString(R.string.email_subject));
-                    //emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, mContext.getString(R.string.email_message, " "));
-                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                    if (hasEmailCC) { emailIntent.putExtra(android.content.Intent.EXTRA_CC, theEmailCC); }
+                    if (hasEmailBCC) { emailIntent.putExtra(android.content.Intent.EXTRA_BCC, theEmailBCC); }
+                    if (hasSubject) { emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, theSubject); }
+                    if (hasBody) { emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, theBody); }
+                    startActivity(Intent.createChooser(emailIntent, "Email..."));
                 } catch (Exception ex) {
                 }
                 return true;
